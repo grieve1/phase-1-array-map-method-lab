5ddd2f6c5e426bfbafe2b7d3257211fcaa36a779
drake bishop <drakemx85@gmail.com>
2023-03-21 13:52:15 -0500

    used array.ap method

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  // Use the map() and replace() methods to create a new array of title-cased tutorial names
+  const titleCasedTutorials = tutorials.map(tutorial => {
+    return tutorial.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+  });
+
+  return titleCasedTutorials;
 }
